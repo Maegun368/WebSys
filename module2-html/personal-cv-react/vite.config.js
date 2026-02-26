1951e9ef1802837d08b60137ae214bb3ca7c7ecf
Maegun <Quitorian.maegunaixel@gmail.com>
2026-02-26 22:12:37 +0800

    fixed base path to root

--- module2-html/personal-cv-react/vite.config.js
+++ module2-html/personal-cv-react/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/WebSys/module2-html/personal-cv-react/',
+  base: '/WebSys/',
   plugins: [react()],
 })
